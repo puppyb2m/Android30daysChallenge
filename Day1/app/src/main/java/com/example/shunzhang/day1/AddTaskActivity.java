b74a09ce9b097302d9baff48252f8fe6ebd49bee
puppyb2m <kanginzz@gmail.com>
2018-05-26 19:58:29 +0800

    Move Activity from one to another
    - onClickListener
    - startActivity with intent

---
+++ Day1/app/src/main/java/com/example/shunzhang/day1/AddTaskActivity.java
@@ -0,0 +1,15 @@
+package com.example.shunzhang.day1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class AddTaskActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.add_task);
+    }
+}
--- Day1/app/src/main/java/com/example/shunzhang/day1/MainActivity.java
+++ Day1/app/src/main/java/com/example/shunzhang/day1/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.shunzhang.day1;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +21,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // floating button
+        FloatingActionButton floatingButton = (FloatingActionButton) findViewById(R.id.fab_add_task);
+        floatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddTaskActivity.class);
+                startActivity(intent);
+            }
+        });
 
         // listview
         RecyclerView listView = (RecyclerView) findViewById(R.id.task_listview);
